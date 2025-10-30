b36ecf04e8bf9de76a6c33e9bbfb537214b79170
NickJApps <nicolasfr74@gmail.com>
2025-10-30 22:05:07 +0300

    clean_code

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-/*
 import { CColor } from '@miroyar/ccolor';
 import User from './render/models/User.js';
+import dotenv from 'dotenv';
+import Math from "./render/models/Math.js";
 
-
+// СColor import and test 
 const cColor = new CColor();
+
+// Extends 
+console.log(cColor.print("Extends class", "BG"));
+
 const user = new User('Ник', 30, 1); 
-
-console.log(cColor.print("application is running", "BG"));
-
 console.log(user.getAge());
 console.log(user.getName());
 console.log(user.getUserId());
 
-require('dotenv').config();
-*/
+// Config import and test
+console.log(cColor.print("Configuration file", "BG"));
 
-import dotenv from 'dotenv';
 dotenv.config();
-
 console.log(process.env.NODE_ENV); // → development
 console.log(process.env.PORT);     // → 4000
 console.log(process.env.DB_HOST);  // → localhost
+
+//Math import an test 
+console.log(cColor.print("Math class", "BG"));
+
+const math = new Math();
+console.log(math.sum(2, 2));
---
+++ render/models/Math.js
@@ -0,0 +1,18 @@
+export default class Math {
+
+    sum(num1, num2){
+        return num1 + num2;
+    }
+
+    mult(num1, num2){
+        return num1 * num2;
+    }
+
+    subtract(num1, num2){
+        return num1 - num2;
+    }
+
+    division(num1, num2){
+        return num1 / num2;
+    }
+}
