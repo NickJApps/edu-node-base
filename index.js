2b1e9af8f553602edca748160b08b2dbbe6db1d0
NickJApps <nicolasfr74@gmail.com>
2025-10-30 20:07:28 +0300

    environment_variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 import { CColor } from '@miroyar/ccolor';
 import User from './render/models/User.js';
 
@@ -10,3 +11,13 @@ console.log(cColor.print("application is running", "BG"));
 console.log(user.getAge());
 console.log(user.getName());
 console.log(user.getUserId());
+
+require('dotenv').config();
+*/
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.NODE_ENV); // → development
+console.log(process.env.PORT);     // → 4000
+console.log(process.env.DB_HOST);  // → localhost
